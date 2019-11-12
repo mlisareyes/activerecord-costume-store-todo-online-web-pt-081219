651b2678685efce19c8a2de41ecaae507ec0d185
Monica R <mlisareyes@gmail.com>
2019-11-12 04:30:08 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,7 +4,7 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
     create_table :costume_stores do |t|
       t.string :name
       t.string :location
-      t.integer :inventory
+      t.integer :costume_inventory
       t.integer :employees_count
       t.boolean :still_in_business
       t.datetime :opening_time
